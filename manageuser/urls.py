a3a90b50bb0ef96380a7f4765b9cca05a411b79d
Dongyeon1201 <dongyeon1201@gmail.com>
2021-09-11 01:12:48 +0900

    edit urls.py 3

--- manageuser/urls.py
+++ manageuser/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('view/all/', UserListView.as_view()),
-    path('view/<int:user_id>/', UserDetailView.as_view()),
-    path('create/', UserCreateView.as_view()),
-    path('update/<int:user_id>/', UserUpdateView.as_view()),
-    path('delete/<int:user_id>/', UserDeleteView.as_view()),
+    path('view/all', UserListView.as_view()),
+    path('view/<int:user_id>', UserDetailView.as_view()),
+    path('create', UserCreateView.as_view()),
+    path('update/<int:user_id>', UserUpdateView.as_view()),
+    path('delete/<int:user_id>', UserDeleteView.as_view()),
 ]
