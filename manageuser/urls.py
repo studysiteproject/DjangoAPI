3112bc14cc670951d31f761015b7ad293a843d22
Dongyeon1201 <dongyeon1201@gmail.com>
2021-09-11 01:03:52 +0900

    edit urls.py

--- manageuser/urls.py
+++ manageuser/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('view/all/', UserListView.as_view()),
     path('view/<int:user_id>/', UserDetailView.as_view()),
     path('create/', UserCreateView.as_view()),
-    path('update/<int:board_id>/', UserUpdateView.as_view()),
-    path('delete/<int:board_id>/', UserDetailView.as_view()),
+    path('update/<int:user_id>/', UserUpdateView.as_view()),
+    path('delete/<int:user_id>/', UserDetailView.as_view()),
 ]
