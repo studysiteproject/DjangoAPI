5a8288c1705d33014131581000d1caf3aaee20ce
Dongyeon1201 <dongyeon1201@gmail.com>
2021-09-11 01:39:50 +0900

    edit settings.py 2

--- apiserver/settings.py
+++ apiserver/settings.py
@@ -32,12 +32,12 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
-    # 'django.contrib.auth',
-    # 'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
-    # 'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     # 'bbs',
     'manageuser',
     'rest_framework',
