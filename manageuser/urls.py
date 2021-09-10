e5888ff21aadc49fd2ce7a8add3be5302f171dc4
Dongyeon1201 <dongyeon1201@gmail.com>
2021-09-11 01:01:53 +0900

    view add in views.py

--- manageuser/urls.py
+++ manageuser/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('view/all/', UserListView.as_view()),
     path('view/<int:user_id>/', UserDetailView.as_view()),
     path('create/', UserCreateView.as_view()),
-    # path('update/<int:board_id>/', BoardUpdateView.as_view()),
-    # path('delete/<int:board_id>/', BoardDeleteView.as_view()),
+    path('update/<int:board_id>/', UserUpdateView.as_view()),
+    path('delete/<int:board_id>/', UserDetailView.as_view()),
 ]
--- manageuser/views.py
+++ manageuser/views.py
@@ -85,3 +85,77 @@ class UserCreateView(APIView):
 
         msg = {'state': 'success'}
         return Response(msg, status=status.HTTP_201_CREATED)
+
+class UserUpdateView(APIView):
+
+    queryset = User.objects.all()
+    pk_url_kwargs = 'user_id'
+
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = self.queryset
+
+        pk = self.kwargs.get(self.pk_url_kwargs)
+        
+        return queryset.filter(pk=pk).first()
+
+    def post(self, request, *args, **kwargs):
+        post_data = {key: request.POST.get(key) for key in request.POST.keys() if key not in ('id', 'warning_cnt', 'account_state')}
+
+        for key in post_data:
+            
+            # 만약 NULL 값이 허용되지 않는 필드일 때
+            if User._meta.get_field(key).empty_strings_allowed == False:
+
+                # 입력된 데이터가 존재하지 않을 때
+                if not post_data[key]:
+                    msg = {'state': 'fail', 'detail': 'No Data For {}'.format(key)}
+                    return Response(msg, status=status.HTTP_400_BAD_REQUEST)
+            
+            # 만약 제한 길이가 존재하는 필드일 때
+            if User._meta.get_field(key).max_length != None:
+
+                # 입력된 데이터가 해당 필드의 제한 길이보다 긴 데이터일 때
+                if len(post_data[key]) > User._meta.get_field(key).max_length:
+                    msg = {'state': 'fail', 'detail': 'input over max length of {}'.format(key)}
+                    return Response(msg, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = self.get_object()
+
+        if not user:
+            msg = {'state': 'fail', 'detail': 'invalid user_id'}
+            return Response(msg, status=status.HTTP_400_BAD_REQUEST)
+        
+        for key, value in post_data.items():
+            setattr(user, key, value)
+        
+        user.save()
+
+        msg = {'state': 'success'}
+        return Response(msg, status=status.HTTP_201_CREATED)
+
+class UserDeleteView(APIView):
+
+    queryset = User.objects.all()
+    pk_url_kwargs = 'user_id'
+
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = self.queryset
+
+        pk = self.kwargs.get(self.pk_url_kwargs)
+        
+        return queryset.filter(pk=pk).first()
+
+    def get(self, request, *args, **kwargs):
+
+        user = self.get_object()
+
+        if not user:
+            msg = {'state': 'fail', 'detail': 'invalid user_id'}
+            return Response(msg, status=status.HTTP_400_BAD_REQUEST)
+
+        user.delete()
+
+        msg = {'state': 'success'}
+        return Response(msg, status=status.HTTP_200_OK)
