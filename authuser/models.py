ddaabcee5c26a8c0c5926d41b951a57a6cef0cba
Dongyeon1201 <dongyeon1201@gmail.com>
2021-09-13 02:07:19 +0900

    edit models.py

--- authuser/models.py
+++ authuser/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Refresh(models.Model):
     id = models.BigAutoField(primary_key=True)
-    user_index = models.ForeignKey("manageuser.User", related_name="userindex", on_delete=models.CASCADE, to_field="id")
+    user_index = models.ForeignKey("manageuser.User", related_name="userindex", on_delete=models.CASCADE, to_field="user_index")
     refresh_token = models.CharField(max_length=1024, null=False)
 
     class Meta:
