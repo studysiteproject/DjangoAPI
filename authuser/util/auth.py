766d5d843c8f6aa9e55ce63c71186d00b13cab15
Dongyeon1201 <dongyeon1201@gmail.com>
2021-09-13 03:35:15 +0900

    edit auth.py in authuser/util 2

--- authuser/util/auth.py
+++ authuser/util/auth.py
@@ -75,8 +75,7 @@ def verify_refresh_token(refresh_token):
 def register_refresh_token(refresh_token, index):
     
     get_user_index = User.objects.get(id=index)
-
-    refresh_object = Refresh.objects.get(user_index=get_user_index)
+    refresh_object = Refresh.objects.get(user_index=int(index))
 
     # 삭제 후 재 생성
     if refresh_object:
