def97a1acef0d0a44a215ca25ff63eb2a937e089
Dongyeon1201 <dongyeon1201@gmail.com>
2021-09-14 20:27:03 +0900

    edit password func

--- manageuser/urls.py
+++ manageuser/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('view/all', UserListView.as_view()),
-    path('view/<int:user_id>', UserDetailView.as_view()),
+    path('allview', UserListView.as_view()),
+    path('view', UserDetailView.as_view()),
     path('create', UserCreateView.as_view()),
-    path('update/<int:user_id>', UserUpdateView.as_view()),
+    path('update', UserUpdateView.as_view()),
     path('delete/<int:user_id>', UserDeleteView.as_view()),
     path('authpage', AuthPage.as_view())
 ]
