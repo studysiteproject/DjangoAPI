b0c687baae1322acfb4d3e3b191bb07177701ff5
Dongyeon1201 <dongyeon1201@gmail.com>
2021-09-14 18:10:37 +0900

    edit authuser

--- manageuser/serializers.py
+++ manageuser/serializers.py
@@ -4,4 +4,5 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        exclude = ['user_pw']
+        # fields = '__all__'
