63a4c85b7174eb6a3487a4f009a65125cad63eaa
Dongyeon1201 <dongyeon1201@gmail.com>
2021-09-15 15:49:17 +0900

    id column -> refresh_token_id rename

--- authuser/models.py
+++ authuser/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Refresh(models.Model):
-    id = models.BigAutoField(primary_key=True)
+    refresh_token_id = models.BigAutoField(primary_key=True)
     user_index = models.ForeignKey("manageuser.User", on_delete=models.CASCADE, db_column="user_index")
     refresh_token = models.CharField(max_length=1024, null=False)
 
