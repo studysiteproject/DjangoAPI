412011c5dda5a6ebca2142ae36ce7cc4f0251b89
Dongyeon1201 <dongyeon1201@gmail.com>
2021-09-16 16:51:45 +0900

    edit urls.py in authuser app

--- authuser/urls.py
+++ authuser/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('login', UserLogin.as_view()),
     path('logout', UserLogout.as_view()),
-    path('id_duplicate_check', UserLogout.as_view()),
+    path('id_duplicate_check', IdDuplicatecheck.as_view()),
     path('verify_user', TockenAuth.as_view()),
 ]
