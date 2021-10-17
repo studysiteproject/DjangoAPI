77b855d16dac1351ebfb94065ebec2fc0a2b87b9
Dongyeon1201 <dongyeon1201@gmail.com>
2021-10-17 20:35:33 +0900

    delete managereport

--- apiserver/settings/base.py
+++ apiserver/settings/base.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.core.mail',
     'manageuser',
-    'managereport',
+    # 'managereport',
     'authuser',
     'rest_framework',
 ]
