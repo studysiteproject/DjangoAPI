153d97c9eba97d386ec5a26fd699c7245cc8ca0a
Dongyeon1201 <dongyeon1201@gmail.com>
2021-10-30 01:05:27 +0900

    create manageprofile app

---
+++ manageprofile/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from manageuser.models import User
+
+# Create your models here.
+class ProfileImage(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column="user_id")
+    img_url = models.CharField(max_length=1024, null=False, default='default.png')
+
+    class Meta:
+        managed = False
+        db_table = 'profile_image'
