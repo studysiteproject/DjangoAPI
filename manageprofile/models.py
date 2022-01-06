0cf2153dda119a32fb8dd8658fa2e538d2a86a91
Dongyeon1201 <dongyeon1201@gmail.com>
2022-01-06 21:42:28 +0900

    edit ProfileImage model

--- manageprofile/models.py
+++ manageprofile/models.py
@@ -3,8 +3,8 @@ from manageuser.models import User
 
 # Create your models here.
 class ProfileImage(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column="user_id")
+    # id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column="user_id", primary_key=True)
     img_url = models.CharField(max_length=1024, null=False, default='default.png')
 
     class Meta:
