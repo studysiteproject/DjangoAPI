fcc9e4550658e1bb9798b73fef41628e369da1d4
Dongyeon1201 <dongyeon1201@gmail.com>
2022-04-30 21:16:50 +0900

    gitignore add testorm

--- testorm/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestormConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'testorm'
--- testorm/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import TESTORM
-
-urlpatterns = [
-    path("test/<int:user_index>", TESTORM.as_view()),
-]
